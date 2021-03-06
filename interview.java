86932e0f2c97fa6ff24e3b69594d5b546325a01f
tyagi917 <tyagipranav000@gmail.com>
2021-03-06 13:35:49 +0530

    first-commit

---
+++ interview.java
@@ -0,0 +1,44 @@
+data= [ 
+
+ {    
+student_data:{      name:'A',      id: 123,      marks:[{'hi':11,'mt':22,'en':20}]    
+}  
+}
+
+,
+  
+{    student_data:{      name:'B',      id: 124,      marks:[{'hi':12,'mt':20,'en':10}]    
+ }  
+}, 
+ 
+{    student_data:{      name:'C',      id: 125,      marks:[{'hi':12,'mt':22,'en':23}]    } 
+ 
+}
+];
+
+Output{  '123': {    name: 'A',    total_marks: 52  },  '124': {    name: 'B',    total_marks: 56  },  '125': {    name: 'C',    total_marks: 57  }}
+
+function input(studentdata){
+const arr=[];
+studentdata.forEach(function(data){
+var sum=0;
+var mark=data.student_data.marks[0].values();
+for(var i=0;i<mark.length;i++){
+sum=sum+mark[i];
+
+}
+let hashMap=new Map();
+hashMap['data.student_data.id']={
+name:'data.student_data.name',
+total_marks:sum;
+
+}
+arr.unshift(hashMap);
+
+
+})
+return arr;
+}
+input(data)
+ 
+
